079d957d5631eeff05a360dd146a651f67f4d844
Theo Gabriel <theonbgabriel@gmail.com>
2025-06-10 14:45:59 -0300

    Implementação da página de doação, e obrigado, adicionei o botao fixado de doar agora, e o link no header, criei um novo arquivo JavaScript para animação na página de doação

---
+++ source/script.js
@@ -0,0 +1,37 @@
+document.getElementById("btnProximo").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const formNome = document.getElementById("formNome");
+  const formEmail = document.getElementById("formEmail");
+  const btnVoltar = document.getElementById("btnVoltar");
+  const btnSalvar = document.getElementById("btnSalvar");
+
+  // Esconder o campo de nome
+  formNome.classList.add("opacity-0", "-translate-y-20", "pointer-events-none");
+  btnVoltar.classList.add("opacity-100");
+  btnSalvar.classList.add("block");
+  btnSalvar.classList.remove("hidden");
+
+  // Mostrar o campo de email
+  formEmail.classList.remove("opacity-0", "translate-y-20", "pointer-events-none");
+  formEmail.classList.add("opacity-100", "translate-y-0");
+});
+
+document.getElementById("btnVoltar").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const formNome = document.getElementById("formNome");
+  const formEmail = document.getElementById("formEmail");
+  const btnVoltar = document.getElementById("btnVoltar");
+  const btnSalvar = document.getElementById("btnSalvar");
+  
+  // Esconder o campo de nome
+  formNome.classList.remove("opacity-0", "-translate-y-20", "pointer-events-none");
+  btnVoltar.classList.remove("opacity-100");
+  btnSalvar.classList.remove("block");
+  btnSalvar.classList.add("hidden");
+
+  // Mostrar o campo de email
+  formEmail.classList.add("opacity-0", "translate-y-20", "pointer-events-none");
+  formEmail.classList.remove("opacity-100", "translate-y-0");
+});
