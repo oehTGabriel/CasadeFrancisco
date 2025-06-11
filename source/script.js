427d5cc8b09fbbc41b3db3832c267472891acd43
Theo Gabriel <theonbgabriel@gmail.com>
2025-06-11 13:23:16 -0300

    resolvendo UX do botao proximo pagina de doação

--- source/script.js
+++ source/script.js
@@ -5,12 +5,16 @@ document.getElementById("btnProximo").addEventListener("click", function (e) {
   const formEmail = document.getElementById("formEmail");
   const btnVoltar = document.getElementById("btnVoltar");
   const btnSalvar = document.getElementById("btnSalvar");
+  const btnProximo = document.getElementById("btnProximo");
+
 
   // Esconder o campo de nome
   formNome.classList.add("opacity-0", "-translate-y-20", "pointer-events-none");
   btnVoltar.classList.add("opacity-100");
   btnSalvar.classList.add("block");
   btnSalvar.classList.remove("hidden");
+  btnProximo.classList.add("hidden");
+
 
   // Mostrar o campo de email
   formEmail.classList.remove("opacity-0", "translate-y-20", "pointer-events-none");
@@ -24,12 +28,15 @@ document.getElementById("btnVoltar").addEventListener("click", function (e) {
   const formEmail = document.getElementById("formEmail");
   const btnVoltar = document.getElementById("btnVoltar");
   const btnSalvar = document.getElementById("btnSalvar");
+  const btnProximo = document.getElementById("btnProximo");
+  
   
   // Esconder o campo de nome
   formNome.classList.remove("opacity-0", "-translate-y-20", "pointer-events-none");
   btnVoltar.classList.remove("opacity-100");
   btnSalvar.classList.remove("block");
   btnSalvar.classList.add("hidden");
+  btnProximo.classList.remove("hidden");
 
   // Mostrar o campo de email
   formEmail.classList.add("opacity-0", "translate-y-20", "pointer-events-none");
